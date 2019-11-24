0cecf300eed8790f3b6dcbeee71e246f1ee4948a
HairMachine <hair.machine@gmail.com>
2019-11-24 10:45:47 +0000

    Zot still allows you to translocate - at least until you have the orb

--- crawl-ref/source/orb.cc
+++ crawl-ref/source/orb.cc
@@ -84,7 +84,7 @@ void orb_pickup_noise(const coord_def& where, int loudness, const char* msg, con
  */
 bool orb_limits_translocation(bool temp)
 {
-    return temp && player_in_branch(BRANCH_ZOT) || player_has_orb();
+    return player_has_orb();
 }
 
 void start_orb_run(game_chapter chapter, const char* message)
